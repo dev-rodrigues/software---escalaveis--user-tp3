37b39287d7f4ac7a055bab40db1e074bcd8c7586
carlos <carlos.hrq.rodrigues@gmail.com>
2022-09-19 16:06:27 -0300

    new role

--- src/main/java/br/com/devrodrigues/userservice/core/Permissions.java
+++ src/main/java/br/com/devrodrigues/userservice/core/Permissions.java
@@ -3,14 +3,16 @@ package br.com.devrodrigues.userservice.core;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.List.of;
+
 public class Permissions {
 
     private Permissions() {
         //no-sonar
     }
     private static final Map<String, List<String>> permissions = Map.of(
-            "TEACHER", List.of("rolls", "reasons", "reports"),
-            "FATHER", List.of("reports")
+            "TEACHER", of("rolls", "reasons", "reports, justify"),
+            "FATHER", of("reports, justify")
     );
 
     public static boolean hasPermission(String role, String function) {
